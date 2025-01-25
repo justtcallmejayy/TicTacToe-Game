b7e2ddeb4794ca1fda0ac0400a7069c5a3d7fa92
JAY CHOKSI <jaychokxii1804@gmail.com>
2025-01-25 00:07:25 -0500

    Added fixed code layout

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
+
 let boxes = document.querySelectorAll(".box");
 let resetButton = document.getElementById("resetButton");
-// let colorButtons = document.querySelectorAll(".colorButton");
-
-// initializing the Boolean for players turn
-let playerTurn0 = true; //For player-X or player-O
-
-//Storing the playerTurn in  2D Array
+let playerTurn = true; // true for Player X, false for Player O
 const winingPatterns = [
   [0, 1, 2],
   [0, 3, 6],
@@ -14,10 +10,54 @@ const winingPatterns = [
   [2, 5, 8],
   [2, 4, 6],
   [3, 4, 5],
-  [7, 6, 8],
+  [6, 7, 8],
 ];
 
-// // Function to change the background color of a box
-// function changeBackgroundColor(box, color) {
-//   box.style.backgroundColor = color;
-// }
+function checkWinner() {
+  for (let pattern of winingPatterns) {
+    let [a, b, c] = pattern;
+    if (
+      boxes[a].innerText !== "" &&
+      boxes[a].innerText === boxes[b].innerText &&
+      boxes[a].innerText === boxes[c].innerText
+    ) {
+      return boxes[a].innerText; // Return the winner (X or O)
+    }
+  }
+  return null;
+}
+
+function isDraw() {
+  return Array.from(boxes).every((box) => box.innerText !== "");
+}
+
+function resetGame() {
+  boxes.forEach((box) => {
+    box.innerText = "";
+    box.classList.remove("disabled");
+  });
+  playerTurn = true;
+}
+
+// Event listeners for boxes
+boxes.forEach((box, index) => {
+  box.addEventListener("click", () => {
+    if (box.innerText === "") {
+      box.innerText = playerTurn ? "X" : "O";
+      box.classList.add("disabled");
+
+      let winner = checkWinner();
+      if (winner) {
+        setTimeout(() => alert(`${winner} wins!`), 100);
+        boxes.forEach((b) => b.classList.add("disabled"));
+      } else if (isDraw()) {
+        setTimeout(() => alert("It's a draw!"), 100);
+      } else {
+        playerTurn = !playerTurn; // Switch turn
+      }
+    }
+  });
+});
+
+// Event listener for reset button
+resetButton.addEventListener("click", resetGame);
